a56fe3e377e87ef64e66af9450d1300f9ca2eef0
IcyStab <87219008+IcyStab@users.noreply.github.com>
2023-02-27 00:52:04 -0600

    fixed picture bug

--- Property-Website/website/AvailableUnits/models.py
+++ Property-Website/website/AvailableUnits/models.py
@@ -10,12 +10,18 @@ STATUS = (
     (1,"Publish")
 )
 
+AVAILABLE = (
+     (0,"Available"),
+     (1,"Not Available")
+)
+
 class Listings(models.Model):
     address = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=40, unique=True)
     content = models.TextField()
     pictures = models.ImageField(upload_to='images/',default='default.jpg')
     created_on = models.DateTimeField(auto_now_add=True)
+    # available = models.IntegerField(choices=AVAILABLE, default=1)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
--- Property-Website/website/website/urls.py
+++ Property-Website/website/website/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,5 @@ urlpatterns = [
     path('testing', include('AvailableUnits.urls')),
     path('about', include('about.urls'))
 ]
-if settings.DEBUG:  # new
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
